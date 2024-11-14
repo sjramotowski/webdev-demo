1573d029b944a32e4ad54a088a5ac8d08e6a562a
sjramotowski <sjramotowski@email.wm.edu>
2024-11-13 22:10:55 -0500

    Finished tutorial

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.getElementById("submitButton").addEventListener("click", addTask);
 
+function addTask(){
+    const taskInput = document.getElementById('taskInput').value;
+    if(taskInput){
+        const newTask = document.createElement('li');
+
+        newTask.innerText= taskInput;
+
+        const taskList = document.getElementById('taskList');
+        taskList.appendChild(newTask)
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.className="button";
+        deleteButton.innerText="x";
+        newTask.appendChild(deleteButton)
+        deleteButton.addEventListener("click", () => taskList.removeChild(newTask));
+
+        document.getElementById('taskInput').value=''
+    }
+}
